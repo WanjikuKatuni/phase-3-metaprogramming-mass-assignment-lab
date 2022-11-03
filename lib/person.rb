9ba09bccfb15f698950e25c66b7f1b2c452811fd
wanjiku <annritawanjiku77@gmail.com>
2022-11-03 13:23:42 +0300

    complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
 class Person
   # your code here
+
+  def initialize(args)
+    args.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
+
+
 end
+
+
+
+bob_attributes = {name: "bob", hair_color:"brown"}
+
+bob = Person.new(bob_attributes)
+puts bob.name
+puts bob.hair_color
+
+
+susan_attributes = {name: "susan", height: 5.2, eyecolor:"brown"}
+
+susan = Person.new(susan_attributes)
+puts susan.name
+puts susan.height
+puts susan.eyecolor
